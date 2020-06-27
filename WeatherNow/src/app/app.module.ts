144f42e31d85dd6902c89d1515d669325efe7ee3
Sebastian-Ene <ene.sebastian.2000@gmail.com>
2020-06-27 22:27:49 +0300

    Fetch based api calls

--- WeatherNow/src/app/app.module.ts
+++ WeatherNow/src/app/app.module.ts
@@ -1,11 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { WelcomePageComponent } from './welcome-page/welcome-page.component';
 import { MainPageComponent } from './main-page/main-page.component';
-import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -16,7 +14,6 @@ import { HttpClientModule } from '@angular/common/http';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- WeatherNow/src/app/main-page/main-page.component.ts
+++ WeatherNow/src/app/main-page/main-page.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { CityWeatherService } from '../city-weather.service';
+
 
 @Component({
   selector: 'app-main-page',
@@ -9,20 +9,41 @@ import { CityWeatherService } from '../city-weather.service';
 export class MainPageComponent implements OnInit {
 
   city: string = '';
-  cityWeather: object = {};
+  cityWeather = {
+    temp: undefined,
+    status: undefined,
+  };
+  cityUrl: string;
 
 
-  constructor(private CityWS: CityWeatherService) { }
+  constructor() { }
 
   ngOnInit(): void { }
 
   getCity(city: string) {
     this.city = city;
+    this.cityUrl = 'https://openweathermap.org/data/2.5/weather?q=' + this.city + '&appid=439d4b804bc8187953eb36d2a8c26a02';
+    console.log(this.cityUrl);
+
   }
 
-  getCityWeather() {
-    this.CityWS.getWeather().subscribe(vare => this.cityWeather = vare);//need to use a promise here so it won't console.log before the subscription
-    console.log(this.cityWeather);
+  getCityWeather(url) {
+    this.getWeather(url)
+  }
+
+  getWeather(url: string) {
+
+    fetch(url).then(res => {
+      if (res.ok) return res.json()
+      else (alert("City name is wrong or server not working!"))
+    })
+      .then(data => {
+        console.log(data)
+        this.cityWeather.temp = Math.floor(data.main.temp);
+        this.cityWeather.status = data.weather[0].description;
+      })
+      .catch(() => alert('City name is wrong!'));
+
   }
 
 }
